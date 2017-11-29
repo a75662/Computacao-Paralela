6773e9f9070a04eb5f1d4007c38d0255a7261b01
FabioBaiao <a75662@alunos.uminho.pt>
2017-11-29 14:25:25 +0000

    Segunda implementacao sequencial

--- radixsort.c
+++ radixsort.c
@@ -2,38 +2,33 @@
 #include <math.h>
 #include <stdio.h>
 
-
-
-
 void radixsort(int array[], int size, int digit) {
 
 	if(size <= 1 || digit < 0){
 		return;
 	}
 
-	int *newarray[10];
-	int newsize[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int count[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, size};
+	int *aux = malloc(sizeof(int) * size);
+
+	int i, num = pow(10, digit);
+
+	for(i = 0; i < size; i++)
+		count[(array[i]/num) % 10]++;
+
+	for(i = 1; i < 10; i++)
+		count[i] += count[i-1];
+
+	for(i = 0; i < size; i++)
+		aux[--count[(array[i]/num) % 10]] = array[i];
+
+	for(i = 0; i < size; i++)
+		array[i] = aux[i];
+
+	free(aux);
+
 	for(int i = 0; i < 10; i++){
-		newarray[i] = malloc(sizeof(int) * size);
-	}
-
-	for(int i = 0; i < size; i++){
-		int s = pow(10, digit);
-		int bucket = (array[i]/s)%10;
-
-		newarray[bucket][newsize[bucket]++] = array[i];
-	}
-
-	int j = 0;
-	for(int i = 0; i < 10; i++){
-		
-		radixsort(newarray[i], newsize[i], digit-1);
-
-		for(int k = 0; k < newsize[i]; k++){
-			array[j++] = newarray[i][k];
-		}
-
-		free(newarray[i]);
+		radixsort(array + count[i], count[i+1]-count[i], digit-1);
 	}
 }
 
@@ -54,13 +49,15 @@ int sorted(int array[], int size){
 
 int main(int argc, char const *argv[])
 {
-	int size = 10000000;
+	int size = 100000000;
 	int *array = malloc(sizeof(int) * size);
 	for(int i = 0; i < size; i++){
-		array[i] = rand()%100000000;
+		array[i] = rand()%1000000000;
 	}
 	printf("Sorting\n");
-	radixsort(array, size, 7);
+
+	radixsort(array, size, 8);
+
 	if (sorted(array, size)){
 		printf("Sorted\n");
 	}
@@ -69,3 +66,5 @@ int main(int argc, char const *argv[])
 	}
 	return 0;
 }
+
+
