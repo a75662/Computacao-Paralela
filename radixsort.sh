0b471b0e73cfb2302bf3780d685b057aa88d4a91
miguelsilva96 <a74601@alunos.uminho.pt>
2017-11-29 16:28:25 +0000

    SeARCH tests for 100million input

--- radixsort.sh
+++ radixsort.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+max_threads=16
 if [[ $# != 0 ]]; then
-	export OMP_NUM_THREADS=$1
+	max_threads=$1
 fi
-# para cada cut-off, fazer 10 repeticoes
-for (( cut_off = 8; cut_off >= 0; cut_off-- )); do
+cut_off=6
+
+# para cada nr threads, para cada cut-off, fazer 10 repeticoes
+for (( threads = 1; threads <= max_threads; threads *= 2 )); do
+	export OMP_NUM_THREADS=	 $threads
 	for (( rep = 0; rep < 10; rep++ )); do
 		echo ---------------------------------------
 		./a.out -co $cut_off
