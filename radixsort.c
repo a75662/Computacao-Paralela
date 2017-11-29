eafc4fa4dc8bf5c323c89cac76ff61c3dcc990d1
FabioBaiao <a75662@alunos.uminho.pt>
2017-11-29 14:28:01 +0000

    Implementacao paralela

--- radixsort.c
+++ radixsort.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <omp.h>
+#include <string.h>
 
-void radixsort(int array[], int size, int digit) {
+int cut_off;
+
+void cutoffradixsort(int array[], int size, int digit) {
 
 	if(size <= 1 || digit < 0){
 		return;
@@ -28,7 +32,100 @@ void radixsort(int array[], int size, int digit) {
 	free(aux);
 
 	for(int i = 0; i < 10; i++){
+		cutoffradixsort(array + count[i], count[i+1]-count[i], digit-1);
+	}
+}
+
+void radixsort(int array[], int size, int digit) {
+
+	if(size <= 1 || digit < 0){
+		return;
+	}
+
+	if (digit < cut_off) {
+		return cutoffradixsort(array, size, digit);
+	}
+
+	int count[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, size};
+	int *aux = malloc(sizeof(int) * size);
+
+	int i, num = pow(10, digit);
+
+	for(i = 0; i < size; i++)
+		count[(array[i]/num) % 10]++;
+
+	for(i = 1; i < 10; i++)
+		count[i] += count[i-1];
+
+	for(i = 0; i < size; i++)
+		aux[--count[(array[i]/num) % 10]] = array[i];
+
+	for(i = 0; i < size; i++)
+		array[i] = aux[i];
+
+	free(aux);
+
+	for(int i = 0; i < 10; i++){
+		#pragma omp task
 		radixsort(array + count[i], count[i+1]-count[i], digit-1);
+	}
+	#pragma omp taskwait
+}
+
+void firstradixsort(int array[], int size, int digit) {
+
+	if(size <= 1 || digit < 0){
+		return;
+	}
+
+	if (digit < cut_off) {
+		return cutoffradixsort(array, size, digit);
+	}
+
+	int count[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, size};
+	int *aux = malloc(sizeof(int) * size);
+
+	int i, num = pow(10, digit);
+
+	#pragma omp parallel
+	{
+
+		#pragma omp for reduction(+:count)
+		//#pragma omp single
+		for(i = 0; i < size; i++){
+			int c = (array[i]/num) % 10;
+			count[c]++;
+		}
+
+		#pragma omp single
+		for(i = 1; i < 10; i++)
+			count[i] += count[i-1];
+
+		//#pragma omp for
+		#pragma omp single
+		for(i = 0; i < size; i++){
+			int c = (array[i]/num) % 10;
+
+			int ind;
+			//#pragma omp atomic capture
+			ind = --count[c];
+
+			aux[ind] = array[i];
+		}
+
+		#pragma omp for
+		for(i = 0; i < size; i++)
+			array[i] = aux[i];
+
+		#pragma omp single
+		free(aux);
+
+		#pragma omp single
+		for(int i = 0; i < 10; i++){
+			#pragma omp task
+			radixsort(array + count[i], count[i+1]-count[i], digit-1);
+		}
+		#pragma omp taskwait
 	}
 }
 
@@ -47,24 +144,71 @@ int sorted(int array[], int size){
 	return 1;
 }
 
+void parse_args(int argc, const char *argv[], int *digit, int *size) {
+	*digit = 8;
+	*size = pow(10, 8);
+	cut_off = 8;
+	if (argc > 2) {
+		if (strcmp(argv[1], "-d") == 0) {
+			*digit = atoi(argv[2]);
+		}
+		else if (strcmp(argv[1], "-s") == 0) {
+			*size = atoi(argv[2]);
+		}
+		else if (strcmp(argv[1], "-co") == 0) {
+			cut_off = atoi(argv[2]);
+		}
+	}
+	if (argc > 4) {
+		if (strcmp(argv[3], "-d") == 0) {
+			*digit = atoi(argv[4]);
+		}
+		else if (strcmp(argv[3], "-s") == 0) {
+			*size = atoi(argv[4]);
+		}
+		else if (strcmp(argv[3], "-co") == 0) {
+			cut_off = atoi(argv[4]);
+		}
+	}
+	if (argc > 6) {
+		if (strcmp(argv[5], "-d") == 0) {
+			*digit = atoi(argv[6]);
+		}
+		else if (strcmp(argv[5], "-s") == 0) {
+			*size = atoi(argv[6]);
+		}
+		else if (strcmp(argv[5], "-co") == 0) {
+			cut_off = atoi(argv[6]);
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	int size = 100000000;
-	int *array = malloc(sizeof(int) * size);
-	for(int i = 0; i < size; i++){
-		array[i] = rand()%1000000000;
-	}
-	printf("Sorting\n");
+	int digit, size;
+	parse_args(argc, argv, &digit, &size);
 
-	radixsort(array, size, 8);
+	int *array = malloc(sizeof(int) * size);
+
+	int q = pow(10, digit+1);
+	for(int i = 0; i < size; i++){
+		array[i] = rand()%q;
+	}
+
+	#pragma omp parallel
+	#pragma omp single
+	printf("Sorting with %d thread(s)\n", omp_get_num_threads());
+	printf("Cut-off: %d\n", cut_off);
+
+	double it = omp_get_wtime();
+	firstradixsort(array, size, digit);
+	double ft = omp_get_wtime();
 
 	if (sorted(array, size)){
-		printf("Sorted\n");
+		printf("Sorted in %f seconds\n", ft - it);
 	}
 	else {
 		printf("Not Sorted\n");
 	}
 	return 0;
 }
-
-
