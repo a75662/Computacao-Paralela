c893044f5c851f15449a26018818edd6533c23df
FabioBaiao <a75662@alunos.uminho.pt>
2017-11-30 00:50:31 +0000

    Alteracao da ordem de execucao do script

--- radixsort.sh
+++ radixsort.sh
@@ -4,14 +4,16 @@ if [[ $# != 0 ]]; then
 	export OMP_NUM_THREADS=$1
 fi
 
-printf Threads,"Cut-Off\n"
-for (( num_threads = 1; num_threads <= 8; num_threads *= 2)); do
-	export OMP_NUM_THREADS=$num_threads
-	for (( cut_off = 3; cut_off > 0; cut_off-- )); do
-		printf $num_threads,$cut_off
-		for (( rep = 0; rep < 10; rep++ )); do
-			printf ","
-			./a.out -co $cut_off -s 8000 -d 3
+printf "Cut-Off","1 Thread","2 Threads","4 Threads","8 Threads\n"
+
+for (( rep = 0; rep < 10; rep++ )); do
+	for (( cut_off = 500; cut_off <= 1024000; cut_off*=2 )); do
+		printf $cut_off
+		for (( num_threads = 1; num_threads <= 8; num_threads*=2 )); do
+			export OMP_NUM_THREADS=$num_threads
+			printf ,
+			# digit = 6 -> 0-10M
+			./a.out -co $cut_off -s $((1500000)) -d 6
 		done
 		printf "\n"
 	done
