0ad0228ce84c1197fe71ba7a0d4f77fcac3daaf2
miguelsilva96 <a74601@alunos.uminho.pt>
2017-12-01 03:20:52 +0000

    100 million input test

--- radixsort.sh
+++ radixsort.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-if [[ $# != 0 ]]; then
-	export OMP_NUM_THREADS=$1
-fi
+#if [[ $# != 0 ]]; then
+#	export OMP_NUM_THREADS=$1
+#fi
 
-printf "Cut-Off","1 Thread","2 Threads","4 Threads","8 Threads\n"
+threads=( 1 2 4 8 10 12 16 20 24 28 32 36 40)
+printf "Cut-Off","1 Thread","2 Threads","4 Threads","8 Threads,"
+printf "10 Thread","12 Threads","16 Threads","20 Threads,"
+printf "24 Thread","28 Threads","32 Threads","36 Threads,"
+printf "40 Threads\n"
 
-for (( rep = 0; rep < 10; rep++ )); do
-	for (( cut_off = 500; cut_off <= 1024000; cut_off*=2 )); do
+for (( rep = 0; rep < 10; rep++ ));
+do
+	for (( cut_off = 125; cut_off <= 75000000; cut_off*=2 ));
+	do #75 000 000 +/- para caber na cache L3 da maquina do search(30Mb, 4bytes por int)
 		printf $cut_off
-		for (( num_threads = 1; num_threads <= 8; num_threads*=2 )); do
+		for num_threads in "${threads[@]}"
+		do
 			export OMP_NUM_THREADS=$num_threads
 			printf ,
 			# digit = 6 -> 0-10M
-			./a.out -co $cut_off -s $((1500000)) -d 6
+			./a.out -co $cut_off -s $((100000000)) -d 7
 		done
 		printf "\n"
 	done
